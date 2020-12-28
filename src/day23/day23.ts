049c258211c3c93660f3645f1f5c74a9465bb8c0
Bruce Lewis <blewis@fitbit.com>
2020-12-28 14:40:01 -0500

    prep for day 23 part 2

--- src/day23/day23.ts
+++ src/day23/day23.ts
@@ -1,41 +1,68 @@
 import * as _ from "lodash";
-import { num } from "../util";
+
+interface Cup {
+  car: number;
+  cdr?: Cup;
+}
 
 export const parse = (lines: string[]) =>
   lines.map((line) => {
     return line.split("").map((str) => parseInt(str));
   });
 
-export const part1 = (data: number[], moveCount: number) => {
-  const cups = [...data];
-  let max = -1;
-  cups.forEach((num) => {
-    if (num > max) {
-      max = num;
-    }
-  });
+const getCircle = (data: number[]): Cup => {
+  let cups: Cup[];
+  cups = data.map((n) => ({ car: n }));
+  for (let i = 0; i < cups.length; i++) {
+    cups[i].cdr = i < cups.length - 1 ? cups[i + 1] : cups[0];
+  }
+  return cups[0];
+};
 
-  const rotate = () => cups.push(cups.shift());
-  const move = () => {
-    let dest = cups[0] - 1;
-    let i = cups.indexOf(dest);
-    while (i < 4) {
-      if (i === -1) {
-        dest = max;
-      } else {
-        dest--;
-      }
-      i = cups.indexOf(dest);
+export const part1 = (data: number[], moveCount: number) => {
+  let cups = getCircle(data);
+  const map: Map<number, Cup> = new Map();
+  map.set(cups.car, cups);
+  let max = cups.car;
+  for (let cup = cups.cdr; cup != cups; cup = cup!.cdr) {
+    map.set(cup!.car, cup!);
+    if (cup!.car > max) {
+      max = cup!.car;
     }
-    const three = cups.splice(1, 3);
-    cups.splice(i - 2, 0, ...three);
+  }
+
+  const rotate = () => {
+    cups = cups!.cdr!;
+  };
+
+  const move = () => {
+    const three = [cups.cdr, cups.cdr!.cdr, cups.cdr!.cdr!.cdr];
+    let dest = cups.car - 1 || max;
+    let destCup = map.get(dest);
+    while (!destCup || three.includes(destCup)) {
+      if (!destCup) {
+        throw new Error(`lost ${dest}`);
+      }
+      if (--dest < 1) {
+        dest = max;
+      }
+      destCup = map.get(dest);
+    }
+    const nextCup = three[2]!.cdr;
+    three[2]!.cdr = destCup!.cdr;
+    destCup!.cdr = three[0];
+    cups.cdr = nextCup;
     rotate();
   };
   for (let i = 1; i <= moveCount; i++) {
     move();
   }
-  let one = cups.indexOf(1);
-  return [...cups.slice(one + 1), ...cups.slice(0, one)].join("");
+  let one = map.get(1);
+  let str = "";
+  for (let cup = one!.cdr; cup !== one; cup = cup!.cdr) {
+    str += cup!.car;
+  }
+  return str;
 };
 
 export const part2 = (data: any[]) => {
